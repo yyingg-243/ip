c2c8083afc13ea2f8f9af2f9d84cbef2bbd08fa1
yyingg-243 <yying2243@gmail.com>
2025-02-03 01:18:05 +0800

    echo what the user inputted
    add line and indentation to make it looks nice
    
    able to echo what the user input

--- src/main/java/ChattyDuke.java
+++ src/main/java/ChattyDuke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ChattyDuke {
     public static void main(String[] args) {
         /*String logo = " ____        _        \n"
@@ -7,9 +9,35 @@ public class ChattyDuke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);*/
 
+        String line = "____________________________________________________________";
+        String indentation = "    ";
+
+        System.out.println(line);
         System.out.println("Hello! I'm ChattyDuke");
         System.out.println("What can I do for you?");
+        System.out.println(line);
         System.out.println();
-        System.out.println("Bye. Hope to see you again soon!");
+
+        Scanner scanner = new Scanner(System.in);
+        Boolean isBye = false;
+
+        while(!isBye){
+            String s1 = scanner.nextLine();
+            System.out.println(indentation + line);
+
+            if(s1.equalsIgnoreCase("bye")){
+                isBye = true;
+                System.out.println(indentation + "Bye. Hope to see you again soon!");
+                System.out.println(indentation + line);
+            }else{
+                System.out.println(indentation + s1);
+                System.out.println(indentation + line);
+                System.out.println();
+
+            }
+
+
+        }
+
     }
 }
