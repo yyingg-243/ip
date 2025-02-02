dc3a7388a2f8d98186dd8107037188c57040e3ec
yyingg-243 <yying2243@gmail.com>
2025-02-03 01:37:23 +0800

    add array to store user's input and print it out
    add an array with a fixed size 100
    
    read in user's input and save to array
    
    able to list out the array
    
    able to exit

--- src/main/java/ChattyDuke.java
+++ src/main/java/ChattyDuke.java
@@ -1,7 +1,11 @@
 import java.util.Scanner;
 
 public class ChattyDuke {
+
+    private static final int MAX_ITEMS = 100;
+
     public static void main(String[] args) {
+
         /*String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -11,6 +15,8 @@ public class ChattyDuke {
 
         String line = "____________________________________________________________";
         String indentation = "    ";
+        String[] items = new String[MAX_ITEMS];
+        int inputCount = 0;
 
         System.out.println(line);
         System.out.println("Hello! I'm ChattyDuke");
@@ -21,16 +27,29 @@ public class ChattyDuke {
         Scanner scanner = new Scanner(System.in);
         Boolean isBye = false;
 
-        while(!isBye){
+        while(!isBye) {
             String s1 = scanner.nextLine();
             System.out.println(indentation + line);
 
-            if(s1.equalsIgnoreCase("bye")){
+            if(s1.equalsIgnoreCase("bye")) {
                 isBye = true;
                 System.out.println(indentation + "Bye. Hope to see you again soon!");
                 System.out.println(indentation + line);
-            }else{
-                System.out.println(indentation + s1);
+            } else if (s1.equalsIgnoreCase("list")) {
+                for(int i = 0; i < inputCount; i++){
+                    int index = i + 1;
+                    System.out.println(indentation + index + ". " + items[i]);
+
+                }
+                System.out.println(indentation + line);
+                System.out.println();
+
+
+            } else {
+                items[inputCount] = s1;
+                inputCount++;
+
+                System.out.println(indentation + "added: " + s1);
                 System.out.println(indentation + line);
                 System.out.println();
 
