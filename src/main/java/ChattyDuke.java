90fcd3b1468b09029aaae46572af8491c1dd00c8
yyingg-243 <yying2243@gmail.com>
2025-02-03 21:26:51 +0800

    remove unnecessary code
    replace some attributes as constant
    
    edit the settings to adhere coding standard

--- src/main/java/ChattyDuke.java
+++ src/main/java/ChattyDuke.java
@@ -2,26 +2,19 @@ import java.util.Scanner;
 
 public class ChattyDuke {
 
-    private static final int MAX_ITEMS = 100;
+    public static final String LINE_SEPARATOR = "____________________________________________________________";
+    public static final int MAX_ITEMS = 100;
+    public static final String INDENTATION = "    ";
 
     public static void main(String[] args) {
 
-        /*String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);*/
-
-        String line = "____________________________________________________________";
-        String indentation = "    ";
         Task[] items = new Task[MAX_ITEMS];
         int inputCount = 0;
 
-        System.out.println(line);
+        System.out.println(LINE_SEPARATOR);
         System.out.println("Hello! I'm ChattyDuke");
         System.out.println("What can I do for you?");
-        System.out.println(line);
+        System.out.println(LINE_SEPARATOR);
         System.out.println();
 
         Scanner scanner = new Scanner(System.in);
@@ -29,21 +22,21 @@ public class ChattyDuke {
 
         while(!isBye) {
             String s1 = scanner.nextLine();
-            System.out.println(indentation + line);
+            System.out.println(INDENTATION + LINE_SEPARATOR);
 
             if(s1.equalsIgnoreCase("bye")) {
                 isBye = true;
-                System.out.println(indentation + "Bye. Hope to see you again soon!");
-                System.out.println(indentation + line);
+                System.out.println(INDENTATION + "Bye. Hope to see you again soon!");
+                System.out.println(INDENTATION + LINE_SEPARATOR);
             } else if (s1.equalsIgnoreCase("list")) {
-                System.out.println(indentation + "Here are the tasks in your list:");
+                System.out.println(INDENTATION + "Here are the tasks in your list:");
                 for(int i = 0; i < inputCount; i++){
                     Task t = items[i];
                     int index = i + 1;
-                    System.out.println(indentation + index + ". [" + t.getStatusIcon() + "] " + t.getDescription());
+                    System.out.println(INDENTATION + index + ". [" + t.getStatusIcon() + "] " + t.getDescription());
 
                 }
-                System.out.println(indentation + line);
+                System.out.println(INDENTATION + LINE_SEPARATOR);
                 System.out.println();
 
 
@@ -54,9 +47,9 @@ public class ChattyDuke {
                 Task t = items[taskNumber - 1];
                 t.unmark();
 
-                System.out.println(indentation + "OK, I've marked this task as not done yet:");
-                System.out.println(indentation + "  [" + t.getStatusIcon() + "] " + t.getDescription());
-                System.out.println(indentation + line);
+                System.out.println(INDENTATION + "OK, I've marked this task as not done yet:");
+                System.out.println(INDENTATION + "  [" + t.getStatusIcon() + "] " + t.getDescription());
+                System.out.println(INDENTATION + LINE_SEPARATOR);
                 System.out.println();
 
 
@@ -67,9 +60,9 @@ public class ChattyDuke {
                 Task t = items[taskNumber - 1];
                 t.markAsDone();
 
-                System.out.println(indentation + "Nice! I've marked this task as done:");
-                System.out.println(indentation + "  [" + t.getStatusIcon() + "] " + t.getDescription());
-                System.out.println(indentation + line);
+                System.out.println(INDENTATION + "Nice! I've marked this task as done:");
+                System.out.println(INDENTATION + "  [" + t.getStatusIcon() + "] " + t.getDescription());
+                System.out.println(INDENTATION + LINE_SEPARATOR);
                 System.out.println();
 
 
@@ -77,8 +70,8 @@ public class ChattyDuke {
                 items[inputCount] = new Task(s1);
                 inputCount++;
 
-                System.out.println(indentation + "added: " + s1);
-                System.out.println(indentation + line);
+                System.out.println(INDENTATION + "added: " + s1);
+                System.out.println(INDENTATION + LINE_SEPARATOR);
                 System.out.println();
 
             }
