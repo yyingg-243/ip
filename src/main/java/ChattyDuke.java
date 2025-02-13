42019da4209817f4d1a5a1257f4db3bcd76aa7cf
yyingg-243 <yying2243@gmail.com>
2025-02-14 02:58:37 +0800

    Convert the if else to switch statement

--- src/main/java/ChattyDuke.java
+++ src/main/java/ChattyDuke.java
@@ -7,60 +7,8 @@ public class ChattyDuke {
     public static final String INDENTATION = "    ";
     public static final String ADDED_TASK = "Got it. I've added this task:";
     public static final String EXIT_STATEMENT = "Bye. Hope to see you again soon!";
-    public static final String INVALID_NUMBER = "Invalid number, please try again!";
     public static int inputCount = 0;
 
-
-    public static void markTask(String s1, Task[] taskLists){
-        String[] splitInput = s1.split(" ");
-        int taskNumber = Integer.parseInt(splitInput[1]);
-
-        if (taskNumber <= 0 || taskNumber > inputCount){
-            System.out.println(INVALID_NUMBER);
-
-        }else{
-            Task t = taskLists[taskNumber - 1];
-            t.markAsDone();
-            System.out.println(INDENTATION + "Nice! I've marked this task as done:");
-            System.out.println(INDENTATION + t);
-
-        }
-
-        System.out.println(INDENTATION + LINE_SEPARATOR);
-        System.out.println();
-    }
-
-    public static void unmarkTask(String s1, Task[] taskLists){
-        String[] splitInput = s1.split(" ");
-
-        int taskNumber = Integer.parseInt(splitInput[1]);
-
-        if (taskNumber <= 0 || taskNumber > inputCount){
-            System.out.println(INVALID_NUMBER);
-
-        }else{
-            Task t = taskLists[taskNumber - 1];
-            t.unmark();
-            System.out.println(INDENTATION + "OK, I've marked this task as not done yet:");
-            System.out.println(INDENTATION + t);
-
-        }
-
-        System.out.println(INDENTATION + LINE_SEPARATOR);
-        System.out.println();
-    }
-
-    public static void listTask(Task[] taskLists){
-        System.out.println(INDENTATION + "Here are the tasks in your list:");
-        for(int i = 0; i < inputCount; i++){
-            Task t = taskLists[i];
-            int index = i + 1;
-            System.out.println(INDENTATION + index + "." + t);
-
-        }
-        System.out.println(INDENTATION + LINE_SEPARATOR);
-        System.out.println();
-    }
 
 
     public static void main(String[] args) {
@@ -77,40 +25,47 @@ public class ChattyDuke {
         Boolean isBye = false;
 
         while(!isBye) {
+            TaskManager taskManager = new TaskManager();
+
             String s1 = scanner.nextLine();
+            String command = s1.split(" ")[0].toUpperCase();
             System.out.println(INDENTATION + LINE_SEPARATOR);
 
-            if(s1.equalsIgnoreCase("bye")) {
-                isBye = true;
-                System.out.println(INDENTATION + EXIT_STATEMENT);
-                System.out.println(INDENTATION + LINE_SEPARATOR);
+            try{
+                TaskCommand taskCommand = TaskCommand.valueOf(command);
 
-            } else if (s1.equalsIgnoreCase("list")) {
-                listTask(taskLists);
+                switch(taskCommand){
+                case BYE:
+                    isBye = true;
+                    System.out.println(INDENTATION + EXIT_STATEMENT);
+                    System.out.println(INDENTATION + LINE_SEPARATOR);
+                    break;
 
-            } else if (s1.toLowerCase().contains("unmark")) {
-                unmarkTask(s1, taskLists);
+                case LIST:
+                    taskManager.listTask(taskLists);
+                    break;
 
-            } else if (s1.toLowerCase().contains("mark")) {
-                markTask(s1, taskLists);
+                case UNMARK:
+                    taskManager.unmarkTask(s1, taskLists);
+                    break;
 
-            } else {
-                TaskManager taskManager = new TaskManager();
+                case MARK:
+                    taskManager.markTask(s1, taskLists);
+                    break;
 
-                if(s1.toLowerCase().startsWith("todo")){
-                    taskManager.todoTask(s1, taskLists);
-
-                }else if (s1.toLowerCase().startsWith("deadline")){
-                    taskManager.deadlineTask(s1, taskLists);
-
-                }else if(s1.toLowerCase().startsWith("event")){
-                    taskManager.eventTask(s1, taskLists);
+                default:
+                    taskManager.handleNewTask(taskCommand, s1, taskLists);
+                    System.out.println(INDENTATION + LINE_SEPARATOR);
+                    System.out.println();
 
                 }
 
+            }catch(IllegalArgumentException e){
+                System.out.println("Please input valid command :)");
                 System.out.println(INDENTATION + LINE_SEPARATOR);
                 System.out.println();
             }
+
         }
     }
 }
